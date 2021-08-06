7248157ae75135e51076e8aebb9860e8ef894bca
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-06 15:33:30 -0600

    Project init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,46 @@
 <template>
   <header>
-    <Navbar />
+    <h1 class="bg-primary text-center">
+      Welcome the Shadab Network
+    </h1>
   </header>
   <main>
-    <router-view />
+    <div class="row">
+      <div class="col-3">
+        <h2 class="ml-5 float-right">
+          login
+        </h2>
+      </div>
+      <div class="col-9 text-center">
+        <div class="row">
+          <div class="col-12 ">
+            <h3>NavBareCommponent</h3>
+          </div>
+          <div class="col">
+            <router-view />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="row d-flex flex-colunm">
+      <section>
+        <div class="col-4">
+          A
+        </div>
+      </section>
+      <section>
+        <div class="col-4">
+          B
+        </div>
+        <div class="col-4">
+          C
+        </div>
+      </section>
+    </div> -->
   </main>
   <footer>
     <div class="bg-dark text-light text-center p-4">
-      Made with 💖 by CodeWorks
+      Made with 💖 by Shadab
     </div>
   </footer>
 </template>
