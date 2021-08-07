d954f26a15da5f30c8170f3945ebf314cd8a0aa1
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-06 22:44:34 -0600

    accountPaged set

---
+++ src/pages/AccountPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ account.name }}</h2>
+    <h2>{{ account.email }}</h2>
+    <img :src="account.image" alt="">
+  </div>
+</template>
+<script>
+import { computed } from 'vue'
+import { AppState } from '../AppState'
+
+export default {
+  name: 'Account',
+  setup() {
+    return {
+      account: computed(() => AppState.account)
+    }
+  }
+}
+</script>
