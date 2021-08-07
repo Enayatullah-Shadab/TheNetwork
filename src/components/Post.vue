249c5387ba233270c6ffd6d18d6cc231824558a0
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-06 20:45:45 -0600

    profile login init

---
+++ src/components/Post.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    this is my post
+    <div>
+      <img src="" alt="profile image">
+    </div>
+    <div class="row">
+      <p> my name</p>
+      <div class="col">
+        <p>my account </p>
+        <p>my github </p>
+        <p>my linkedln </p>
+        <p>my resume </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { computed } from '@vue/runtime-core'
+import { AppState } from '../AppState'
+
+export default {
+  name: 'Login',
+  setup() {
+    // account: computed (() => AppState.account)
+  }
+}
+</script>
